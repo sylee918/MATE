b89803916ebf743c6569caff5cd5eee51c292ec2
Sang-Yun Lee <sangyunlee918@gmail.com>
2024-09-30 15:02:40 -0400

    Add module

--- extra_tools.f90
+++ extra_tools.f90
@@ -1,38 +1,3 @@
-      Module Module_for_NVelocityDirection
-
-      integer :: N_vel_directions
-
-      contains
-
-         Subroutine gen_points_for_NV()
-
-            include "Setting.inc"
-            integer :: nsize, n2
-            integer :: i, j, i0
-            real*8 piset(nTheta), piset2(0:nTheta)
-
-            do i=1,nTheta
-               piset(i)=i*1.d0/(nTheta-1)*pi
-               piset2(i)=i*2.d0/(nTheta-1)*pi
-            enddo
-            piset2(0)=0.d0;
-
-            nsize=2
-            do j=1,nTheta-2
-               n2=nint((nTheta*2-2)*dsin(piset(j)))
-               do i=0,n2-1
-                  nsize=nsize+1
-               enddo
-            enddo
-
-            N_vel_directions = nsize
-
-            return
-         End
-
-      End Module
-
-
       Subroutine gen_points(vel_dir)
          ! Generates points on a sphere at n values of theta.
          ! Points are roughly evenly spaced on the sphere \
