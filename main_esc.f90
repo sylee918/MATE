6d701b1ab7cdb05d22632d96066410adbb4f238c
Sang-Yun Lee <sangyunlee918@gmail.com>
2024-09-30 18:28:07 -0400

    Update main_esc.f90

--- main_esc.f90
+++ main_esc.f90
@@ -9,8 +9,8 @@
       external Init_Parameter, Init_Particles, Trace_particle
       external Get_exobaseBC, read_Lya_Bph, write_density_4D
       external MPI_INIT, MPI_COMM_RANK, MPI_COMM_SIZE, MPI_FINALIZE, MPI_BARRIER, MPI_REDUCE
-      external Forward_Tracing_particle, Calculate_Escaping_Flux_constBC
-!      external Physics_tag, gen_points_for_NV, Make_Parameters_OutFile
+      external Forward_Tracing_particle, Calculate_Escaping_Flux_constBC, Write_ESC_FLUX_2D
+      external Make_Parameters_OutFile
 
       real*8, allocatable, dimension(:,:,:,:) :: b_ptl, f_ptl
       integer, allocatable, dimension(:,:,:) :: b_flags, f_flags
