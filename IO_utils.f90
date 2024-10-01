7312ac98e141fdab220d54d52889b4284aac031b
Sang-Yun Lee <sangyunlee918@gmail.com>
2024-10-01 02:04:01 -0400

    Update IO_utils.f90

--- IO_utils.f90
+++ IO_utils.f90
@@ -239,6 +239,7 @@
 
       Subroutine Write_ESC_FLUX_2D(density_2D,tag)
 
+         use Module_Physics_tag
          include "Setting.inc"
          real*8 density_2D(nbx,nby)
          real, dimension(:,:), allocatable :: real_density_2D
@@ -249,7 +250,7 @@
          allocate(real_density_2D(nbx,nby))
          real_density_2D = real(density_2D)
 
-         filename = trim(outdir) // 'ESC_FLUX_2D' // trim(tag_phys) // '_' // trim(tag) // '.data' 
+         filename = trim(outdir) // 'ESC_FLUX_2D' // '_' // trim(tag_phys) // '_' // trim(tag) // '.data' 
          inquire(iolength=nlen) real_density_2D
          open(file=filename,unit=42,form='unformatted',access='direct',recl=nlen,status='replace')
          write(42,rec=1) real_density_2D
