bfa81297f771d030e980b1fef4686f67c72e3be8
Sang-Yun Lee <sangyunlee918@gmail.com>
2024-10-01 15:03:07 -0400

    rev

--- PBS_MATE1.sh
+++ PBS_MATE1.sh
@@ -1,16 +1,11 @@
 #PBS -S /bin/csh
 #PBS -N slee122
-##PBS -l select=47:ncpus=27:mpiprocs=27:model=bro+1:ncpus=28:mpiprocs=28:model=bro
 #PBS -l select=5:ncpus=24:mpiprocs=24:model=bro+1:ncpus=25:mpiprocs=25:model=bro
-##PBS -l select=11:ncpus=12:mpiprocs=12:model=bro+1:ncpus=13:mpiprocs=13:model=bro
-##PBS -l select=2:ncpus=24:mpiprocs=24:model=bro
-#PBS -l walltime=0:05:00
+#PBS -l walltime=0:10:00
 #PBS -j oe
 #PBS -W group_list=s3015
 #PBS -m e
 #PBS -q devel
-##PBS -q normal
-##PBS -q long
 
 module load comp-intel mpi-hpe
 module load pkgsrc
@@ -20,3 +15,10 @@ cd $PBS_O_WORKDIR
 mpirun -np 145 ./MATE_esc.x
 #mpirun -np 1297 ./MATE.x
 #mpirun -np 48 ./MATE.x
+
+
+##PBS -l select=47:ncpus=27:mpiprocs=27:model=bro+1:ncpus=28:mpiprocs=28:model=bro
+##PBS -l select=11:ncpus=12:mpiprocs=12:model=bro+1:ncpus=13:mpiprocs=13:model=bro
+##PBS -l select=2:ncpus=24:mpiprocs=24:model=bro
+##PBS -q normal
+##PBS -q long
