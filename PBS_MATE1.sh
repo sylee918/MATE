c1f06c23c1c64524769955c41a0ca28368673d60
Sang-Yun Lee <sangyunlee918@gmail.com>
2024-10-01 11:31:27 -0400

    Update PBS_MATE1.sh

--- PBS_MATE1.sh
+++ PBS_MATE1.sh
@@ -1,9 +1,9 @@
 #PBS -S /bin/csh
 #PBS -N slee122
 ##PBS -l select=47:ncpus=27:mpiprocs=27:model=bro+1:ncpus=28:mpiprocs=28:model=bro
-##PBS -l select=5:ncpus=24:mpiprocs=24:model=bro+1:ncpus=25:mpiprocs=25:model=bro
+#PBS -l select=5:ncpus=24:mpiprocs=24:model=bro+1:ncpus=25:mpiprocs=25:model=bro
 ##PBS -l select=11:ncpus=12:mpiprocs=12:model=bro+1:ncpus=13:mpiprocs=13:model=bro
-#PBS -l select=2:ncpus=24:mpiprocs=24:model=bro
+##PBS -l select=2:ncpus=24:mpiprocs=24:model=bro
 #PBS -l walltime=0:05:00
 #PBS -j oe
 #PBS -W group_list=s3015
@@ -17,6 +17,6 @@ module load pkgsrc
 
 cd $PBS_O_WORKDIR
 
-#mpirun -np 145 ./MATE.x
+mpirun -np 145 ./MATE_esc.x
 #mpirun -np 1297 ./MATE.x
-mpirun -np 48 ./MATE.x
+#mpirun -np 48 ./MATE.x
