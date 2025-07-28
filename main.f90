0d4a58ff679e2cfda94506cedea43d408ac40834
Sang-Yun Lee <sangyunlee918@gmail.com>
2025-07-28 16:41:15 -0400

    33

--- main.f90
+++ main.f90
@@ -38,7 +38,9 @@
       call gen_points_for_NV
       call Init_Parameter
       call Get_exobaseBC
-      call read_Lya_Bph  ;  if (i_Photoionization .eq. 0) then; bph = 0.d0; endif
+      call read_Lya_Bph  
+         if (i_Photoionization .eq. 0) then; bph = 0.d0; endif
+         if (ExobaseBC_Model_Name .eq. "CONST") then; Lya = 4.d0; endif
       call Physical_tag
 
 !      call Initialize_Setting
