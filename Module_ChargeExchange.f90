b3a16c844336aff6e6cb9c076ef8557fd9d1c62d
Sang-Yun Lee <sangyunlee918@gmail.com>
2025-08-11 16:32:50 -0400

    99

--- Module_ChargeExchange.f90
+++ Module_ChargeExchange.f90
@@ -48,7 +48,7 @@ contains
       PSD_CX = 0.d0
       do i=1,nstep(iv,iE)
          ICX_i = sum(beta_dt(1:i)) * vsig_1eV
-         PSD_CX = PSD_CX + beta_dt(i) * nH_traj(i) * vsig_1eV * exp(-vel2(i)/cexo2) * fac2 * exp(ICX_i)
+         PSD_CX = PSD_CX + abs(beta_dt(i)) * nH_traj(i) * vsig_1eV * exp(-vel2(i)/cexo2) * fac2 * exp(ICX_i)
       enddo
       ICX = sum(beta_dt) * vsig_1eV
 
@@ -597,10 +597,10 @@ contains
                goto 101
          endif
 
-         !call nearest_grid_plasmasphere(one, nps1)
-         !call nearest_grid_exosphere(one, nH1)
-         call interpolate_plasmasphere(one, nps1)
-         call interpolate_exosphere(one, nH1)
+         call nearest_grid_plasmasphere(one, nps1)
+         call nearest_grid_exosphere(one, nH1)
+         !call interpolate_plasmasphere(one, nps1)
+         !call interpolate_exosphere(one, nH1)
          beta_dt(istep) = nps1*dt
          nH_traj(istep) = nH1
          vel2(istep) = vt**2
@@ -612,10 +612,10 @@ contains
             flag = 1
             call calculate_final_timestep(old,one,dt,f0)
                istep = istep + 1
-               !call nearest_grid_plasmasphere(one, nps1)
-               !call nearest_grid_exosphere(one, nH1)
-               call interpolate_plasmasphere(one, nps1)
-               call interpolate_exosphere(one, nH1)
+               call nearest_grid_plasmasphere(one, nps1)
+               call nearest_grid_exosphere(one, nH1)
+               !call interpolate_plasmasphere(one, nps1)
+               !call interpolate_exosphere(one, nH1)
                beta_dt(istep) = nps1*dt
                nH_traj(istep) = nH1
                vel2(istep) = vt**2
