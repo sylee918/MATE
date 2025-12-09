02e672563d27cc1a239f0cd1104f60adbadf2105
Sang-Yun Lee <sangyunlee918@gmail.com>
2025-12-09 14:21:25 -0500

    RCCX3

--- Module_ChargeExchange.f90
+++ Module_ChargeExchange.f90
@@ -600,6 +600,9 @@ contains
          it_nearest = 24
          iday=iday-1
       endif
+      if (it_nearest < Start_Time_in_YYYYDOY) then
+         it_nearest = Start_Time_in_YYYYDOY
+      endif
 
       ! -----------------------------------------------------------
 
