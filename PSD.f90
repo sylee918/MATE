8eb1ee4471fa3537b7fbe482ea82bd8bc1fec2af
Sang-Yun Lee <sangyunlee918@gmail.com>
2026-03-09 16:14:56 -0400

    0309

--- PSD.f90
+++ PSD.f90
@@ -102,11 +102,7 @@
                vel2 = sum(vel*vel)
 !               if (i_ChargeExchange .eq. 1) then
 !                  call cpu_time(cx_t0)
-<<<<<<< Updated upstream
 !                  call Calculate_ChargeExchange(iE,iv, ptl(iv,iE,:), flags(iv,iE), current_time, ICX, PSD_CX)  !! Already used above?
-=======
-                  call Calculate_ChargeExchange(iE,iv, ptl(iv,iE,:), flags(iv,iE), current_time, ICX, PSD_CX)
->>>>>>> Stashed changes
 !                  call cpu_time(cx_t1)
 !                  cx_time_total = cx_time_total + (cx_t1 - cx_t0)
 !                  cx_calls = cx_calls + 1
